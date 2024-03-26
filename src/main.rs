1a61fadb945dd41ea5a3cba98742665df381b39b
Scooter1337 <lucav007@hotmail.com>
2024-03-27 00:42:50 +0100

    ongekeurd, gekeurd

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use std::fs::File;
 use std::io::{self, BufRead, Write};
 use std::path::Path;
 
-
 fn main() {
+    work("basiswoorden-gekeurd.txt", "semordnilap-gekeurd.txt");
+    work("basiswoorden-ongekeurd.txt", "semordnilap-ongekeurd.txt");
+}
+
+fn work(input: &str, output: &str) {
     // open file wordlist.txt
-    let lines = read_lines("basiswoorden-gekeurd.txt").unwrap();
+    let lines = read_lines(input).unwrap();
 
     // read file line by line, add to set
     let mut set = std::collections::HashSet::new();
@@ -17,7 +21,7 @@ fn main() {
 
     // for each word in set, check if the reverse is in set too, but if reverse is the same as the word, skip
     // save the word and the reverse to file semordnilap.txt
-    let mut semordnilap = File::create("semordnilap-gekeurd.txt").unwrap();
+    let mut semordnilap = File::create(output).unwrap();
     for word in set.iter() {
         let reverse: String = word.chars().rev().collect();
         if word != &reverse && !found.contains(&reverse) && set.contains(&reverse) {
