a3abbe5e4a1d6f49decdceeec0f119aa6bc0691b
Scooter1337 <lucav007@hotmail.com>
2024-03-27 01:10:11 +0100

    comma-seperated, added anagrams

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
 use std::path::Path;
+use std::vec::Vec;
 
 fn main() {
-    semordnilap("basiswoorden-gekeurd.txt", "semordnilap-gekeurd.txt");
-    semordnilap("basiswoorden-ongekeurd.txt", "semordnilap-ongekeurd.txt");
-    palindromes("basiswoorden-gekeurd.txt", "palindromes-gekeurd.txt");
-    palindromes("basiswoorden-ongekeurd.txt", "palindromes-ongekeurd.txt");
+    semordnilap("basiswoorden-gekeurd.txt", "nemordnilap-gekeurd.txt");
+    semordnilap("basiswoorden-ongekeurd.txt", "nemordnilap-ongekeurd.txt");
+    palindromes("basiswoorden-gekeurd.txt", "palindromen-gekeurd.txt");
+    palindromes("basiswoorden-ongekeurd.txt", "palindromen-ongekeurd.txt");
+    anagrams("basiswoorden-gekeurd.txt", "anagrammen-gekeurd.txt");
+    anagrams("basiswoorden-ongekeurd.txt", "anagrammen-ongekeurd.txt");
 }
 
 fn semordnilap(input: &str, output: &str) {
@@ -25,10 +29,15 @@ fn semordnilap(input: &str, output: &str) {
     // save the word and the reverse to file semordnilap.txt
     let mut semordnilap = File::create(output).unwrap();
     for word in set.iter() {
+        if word.len() < 2 { continue; }
+
+        // skip words consisting of 1 character
+        if word.chars().all(|c| c == word.chars().next().unwrap()) { continue; }
+
         let reverse: String = word.chars().rev().collect();
         if word != &reverse && !found.contains(&reverse) && set.contains(&reverse) {
             found.insert(word);
-            writeln!(semordnilap, "{} <> {}", word, reverse).unwrap();
+            writeln!(semordnilap, "{}, {}", word, reverse).unwrap();
         }
     }
 }
@@ -46,10 +55,50 @@ fn palindromes(input: &str, output: &str) {
     // for each word in set, check if the reverse is the same as the word
     let mut palindromes = File::create(output).unwrap();
     for word in set.iter() {
+
+        // skip words with less than 2 characters
+        if word.len() < 2 { continue; }
+
+        // skip words consisting of 1 character
+        if word.chars().all(|c| c == word.chars().next().unwrap()) { continue; }
+
         let reverse: String = word.chars().rev().collect();
         if word == &reverse {
             writeln!(palindromes, "{}", word).unwrap();
         }
+    }
+}
+
+fn anagrams(input: &str, output: &str) {
+    // open file wordlist.txt
+    let lines = read_lines(input).unwrap();
+
+    // read file line by line, add to map (word, sorted word)
+    let mut map: HashMap<String, Vec<String>> = std::collections::HashMap::new();
+    for line in lines {
+        let word = line.unwrap().to_lowercase();
+        let sorted = sort_word(&word);
+        let wordlist = map.get_mut(&sorted);
+
+        match wordlist {
+            Some(words) => {
+                words.push(word);
+            },
+            None => {
+                map.insert(sorted, vec![word]);
+            }
+        }
+    }
+
+    // for each sorted word in map, if the length of the list of words is greater than 1, save to file anagrammen.txt
+    let mut anagrams = File::create(output).unwrap();
+
+    for (_, words) in map.iter() {
+        if words.len() > 1 {
+            let words_joined = words.join(", ");
+            writeln!(anagrams, "{}", words_joined).unwrap();
+        }
+
     }
 }
 
@@ -60,4 +109,10 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn sort_word(word: &str) -> String {
+    let mut chars: Vec<char> = word.chars().collect();
+    chars.sort();
+    chars.into_iter().collect()
 }
